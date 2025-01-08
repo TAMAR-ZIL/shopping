3158133424f53df2227f54cc44388404a4189787
tamar <t0527199526@gmail.com>
2025-01-08 14:03:25 +0200

    tenth

--- controler/user.js
+++ controler/user.js
@@ -9,27 +9,27 @@ export const getAllUsers=async(req,res)=>{
     res.json(users);
   }
   catch(err){
-    res.status(404).json({tytle:"cant find all users",message:err.message})
+    res.status(404).json({title:"cant find all users",message:err.message})
   }  
 }
 export const getUserById=async(req,res)=>{
     let {id}=req.params;
     if(!isValidObjectId(id))
-        return res.status(404).json({tytle:"invalid code",message:"this is not a correct code"})
+        return res.status(404).json({title:"invalid code",message:"this is not a correct code"})
     try{
       let data=await userModel.findById(id,{password:0});
       if(!data)
-        return res.status(404).json({tytle:"invalid id",message:"no such id"})
+        return res.status(404).json({title:"invalid id",message:"no such id"})
       res.json(data);
     }
     catch(err){
-      res.status(400).json({tytle:"cant get by code",message:err.message})
+      res.status(400).json({title:"cant get by code",message:err.message})
     }  
 }
 export const signUp=async(req,res)=>{
     let{body}=req;
     if(!body.userName||!body.email||!body.password)
-        return res.status(404).json({tytle:"details required",message:"missing details"})
+        return res.status(404).json({title:"details required",message:"missing details"})
     if(body.userName.length <= 2)
         return res.status(400).json({title:"cannt sign", massage: "name is too short"})
     if(body.password.length <= 7)
@@ -41,23 +41,23 @@ export const signUp=async(req,res)=>{
         res.json(user)
     }
     catch(err){
-        res.status(400).json({tytle:"cant sign up",message:err.message})
+        res.status(400).json({title:"cant sign up",message:err.message})
     }
 }
 export const updateUserById=async (req,res)=>{
     let{id}=req.params;
     let{userName,email}=req.body;
     if(!isValidObjectId)
-        return res.status(404).json({tytle:"invalid id",message:"code isnt correct"})
+        return res.status(404).json({title:"invalid id",message:"code isnt correct"})
     if(userName.length<2||email.length<2)
-        return res.status(404).json({tytle:"uncorrect detail",message:"name or email is too short"})
+        return res.status(404).json({title:"uncorrect detail",message:"name or email is too short"})
     try{
         let user=await userModel.findByIdAndUpdate(id,userName,email,{new:true})
         if(!user)
-            return res.status(404).json({tytle:"cant update this user",message:"no such user with such code"})
+            return res.status(404).json({title:"cant update this user",message:"no such user with such code"})
     }
     catch(err){
-        res.status(400).json({tytle:"cannt update user",message:err.massage})
+        res.status(400).json({title:"cannt update user",message:err.massage})
     }
 }
 
@@ -65,28 +65,28 @@ export const updateUserPassword=async (req,res)=>{
     let{id}=req.params;
     let{password}=req.body;
     if(!isValidObjectId)
-        return res.status(404).json({tytle:"invalid id",message:"code isnt correct"})
+        return res.status(404).json({title:"invalid id",message:"code isnt correct"})
     if(password.length<=7)
-        return res.status(404).json({tytle:"uncorrect detail",message:"password too short"})
+        return res.status(404).json({title:"uncorrect detail",message:"password too short"})
     try{
         let user=await userModel.findByIdAndUpdate(id,password,{new:true})
         if(!user)
-            return res.status(404).json({tytle:"cant update this user",message:"no such user with such code"})
+            return res.status(404).json({title:"cant update this user",message:"no such user with such code"})
     }
     catch(err){
-        res.status(400).json({tytle:"cannt update user",message:err.massage})
+        res.status(400).json({title:"cannt update user",message:err.massage})
     }
 }
 export const login=async(req,res)=>{
     let {id,password}=req.params;
     if(!isValidObjectId(id))
-        return res.status(404).json({tytle:"invalid code",message:"this is not a correct code"})
+        return res.status(404).json({title:"invalid code",message:"this is not a correct code"})
     try{
         let data=await userModel.findOne({_id:id,password});
         res.json(data);
     }
     catch(err){
-        res.status(400).json({tytle:"cannt login",message:err.massage})
+        res.status(400).json({title:"cannt login",message:err.massage})
     }
 }
 
