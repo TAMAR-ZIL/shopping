9ec46227cb29a83d51e9eef13715c5a2c603d19b
tamar <t0527199526@gmail.com>
2025-02-20 01:53:41 +0200

    adds

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ import cors from "cors"
 
 dotenv.config();
 connectDB();
+const express = require('express');
 const app = express();
+const path = require('path');
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(cors());
 app.use(express.json());
 app.use('/api/product',productRouter);
