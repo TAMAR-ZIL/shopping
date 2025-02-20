0ccd00f31485a52dbb03982f7b83099512ba9eaf
tamar <t0527199526@gmail.com>
2025-02-20 23:36:50 +0200

    change the limit router again

--- router/product.js
+++ router/product.js
@@ -3,10 +3,11 @@ import express, { Router } from "express"
 import{addProduct,deleteProductById,getAllProducts,getProductById,updateProductById}from"../controler/product.js"
 
 const router=Router();
+router.get('/totalPages', getTotalPages)
 router.get('/',getAllProducts);
 router.get('/:id',getProductById);
 router.post('/',addProduct);
 router.delete('/:id',deleteProductById);
 router.put('/:id',updateProductById);
-router.get('/totalPages', getTotalPages)
+
 export default router;
