513615d5d6102c756f329b8528f11bb60e8f7914
tamar <t0527199526@gmail.com>
2025-03-09 02:32:40 +0200

    remove a function

--- model/order.js
+++ model/order.js
@@ -7,7 +7,7 @@ const orderSchema = Joi.object({
     date: Joi.date().default(() => new Date()),
     buyingDate: Joi.date().default(() => new Date()),
     address: Joi.string().min(3).max(30).required(),
-    codeUser: Joi.objectId().required(), 
+    codeUser: Joi.required(), 
     product: productSchema, 
     onWay: Joi.boolean().default(false),
     Price: Joi.number().positive().precision(2).required(),
