1783e73ad45d1cc78467c91c64c3af7903096da8
tamar <t0527199526@gmail.com>
2025-03-09 02:25:11 +0200

    export productSchema

--- model/product.js
+++ model/product.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
-const productSchema = new mongoose.Schema({
+import {model,Schema} from "mongoose";
+const productSchema = new Schema({
     nameProduct: { type: String, required: true, minlength: 2 },
     color: { type: String, required: true },
     price: { type: Number, required: true, min: 0 },
     stock: { type: Number, required: true, min: 0 }
 }, { timestamps: true });
-
-export const productModel = mongoose.model("Product", productSchema);
+export { productSchema };
+export const productModel = model("Product", productSchema);
