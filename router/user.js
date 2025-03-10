cc6ac8996a54c4c2dbc134f38ba1ccd2517f6549
tamar <t0527199526@gmail.com>
2025-03-10 11:27:58 +0200

    change user route

--- router/user.js
+++ router/user.js
@@ -7,7 +7,7 @@ const router=Router();
 router.get('/',checkManager,getAllUsers);
 router.get('/:id',checkMiddleware,getUserById);
 router.post('/login',checkMiddleware,login);
-router.post('/',signUp);
+router.post('/signUp',signUp);
 router.put('/:id',checkMiddleware,updateUserById);
 router.put('/:id',checkMiddleware,updateUserPassword);
 
