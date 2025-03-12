2dc668643134c0bc7fb5057b1037fac6b9c240dd
tamar <t0527199526@gmail.com>
2025-03-12 02:51:07 +0200

    change manger middleware

--- middlewares/check.js
+++ middlewares/check.js
@@ -14,20 +14,19 @@ export const checkMiddleware=(req,res,next)=>{
         return res.status(401).json({title:"טרם הצלחנו לאמת את זהותך",message:"בצע כניסה"})
     }    
 }
-export const checkManager=(req,res,next)=>{
-   let token=req.headers.authorization;
-   if(!token)
-     return res.status(401).json({title:"משתמש לא זוהה",message:"בצע כניסה"}) 
-   token=token.split(" ")[1];
-   try{
-    let result=jwt.verify(token,process.env.TOKEN_KEY)
-    if(result.role==='ADMIN')
-        next();
-    return res.status(403).json({title:"אין לך הרשאה",message:"רק מנהל יכול לגשת לכאן"})
-   }
-   catch(err){
-    console.log(err);
-    return res.status(500).json({title:"טרם הצלחנו לאמת את זהותך",message:"בצע כניסה"})
-    
-   }
-}
+export const checkManager = (req, res, next) => {
+    let token = req.headers.authorization;
+    if (!token)
+        return res.status(401).json({ title: "משתמש לא זוהה", message: "בצע כניסה" });
+    token = token.split(" ")[1]; 
+    try {
+        const result = jwt.verify(token, process.env.TOKEN_KEY); 
+        if (result.role === 'ADMIN') {
+            return next(); 
+        }
+        return res.status(403).json({ title: "אין לך הרשאה", message: "רק מנהל יכול לגשת לכאן" });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ title: "טרם הצלחנו לאמת את זהותך", message: "בצע כניסה" });
+    }
+};
