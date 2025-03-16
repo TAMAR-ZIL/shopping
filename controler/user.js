5e22e5f57d62cf9a752bf39ccb7803fc281778eb
tamar <t0527199526@gmail.com>
2025-03-16 22:56:55 +0200

    add log to console for token

--- controler/user.js
+++ controler/user.js
@@ -38,6 +38,7 @@ export const signUp = async (req, res) => {
         const newUser = new userModel({ userName, email, password: hashedPassword, role });
         await newUser.save();
         const token = generateToken(newUser);
+        console.log("Generated Token:", token);
         res.status(201).json({ message: "נרשמת בהצלחה!", token, user: { _id: newUser._id.toString(), userName: newUser.userName, role: newUser.role } });
     } catch (error) {
         console.error("Error during signup:", error);
