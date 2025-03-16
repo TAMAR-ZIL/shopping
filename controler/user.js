cfc4feee9a8978e36f2837be6f71c1ddc56fd3ac
tamar <t0527199526@gmail.com>
2025-03-16 14:28:30 +0200

    change user to newuser

--- controler/user.js
+++ controler/user.js
@@ -38,7 +38,7 @@ export const signUp = async (req, res) => {
         const newUser = new userModel({ userName, email, password: hashedPassword, role });
         await newUser.save();
         const token = generateToken(newUser);
-        res.status(201).json({ message: "נרשמת בהצלחה!", token, user: { id: user._id.toString(), userName: newUser.userName, role: newUser.role } });
+        res.status(201).json({ message: "נרשמת בהצלחה!", token, user: { id: newUser._id.toString(), userName: newUser.userName, role: newUser.role } });
     } catch (error) {
         console.error("Error during signup:", error);
         res.status(500).json({ message: " !שגיאה בשרת" });
