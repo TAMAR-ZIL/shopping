951b8f33fb9e370eeb85e3c9de177ddd65a9042a
tamar <t0527199526@gmail.com>
2025-03-25 20:50:45 +0200

    add emails

--- router/order.js
+++ router/order.js
@@ -1,6 +1,6 @@
 import  { Router } from"express"
 
-import{getEmail,addOrder,deleteOrderById,getAllOrders,getByUserId,updateOrder}from"../controler/order.js"
+import{addOrder,deleteOrderById,getAllOrders,getByUserId,updateOrder}from"../controler/order.js"
 import { checkManager, checkMiddleware } from "../middlewares/check.js";
 
 const router=Router();
@@ -9,6 +9,6 @@ router.get("/:userId",checkMiddleware,getByUserId);
 router.post("/",checkMiddleware,addOrder);
 router.delete("/:id",checkManager,deleteOrderById);
 router.put("/:id",checkManager,updateOrder);
-router.post("/getEmail",getEmail);
+
 
 export default router;
